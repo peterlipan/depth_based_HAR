3d106961e0ec58bc33f9bcc837b5e8b7a32195bc
johnP-Li <847041172@qq.com>
2023-03-24 17:39:57 +0800

    Rewrite the Segmented sampling

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,9 @@
-import torch.utils.data as data
-
-from PIL import Image
 import os
 import os.path
-import numpy as np
 import pandas as pd
-from numpy.random import randint
+from PIL import Image
+import torch.utils.data as data
+from .samplers import SegmentedSample
 
 
 class VideoRecord(object):
@@ -31,7 +29,7 @@ class TSNDataSet(data.Dataset):
     def __init__(self, data_path, csv_path,
                  num_segments=3, new_length=1, modality='depth',
                  image_tmpl='MDepth-{:08d}.png', transform=None,
-                 random_shift=True, test_mode=False):
+                 test_mode=False, start_index=1):
 
         self.data_path = data_path
         self.csv = pd.read_csv(csv_path)
@@ -41,11 +39,11 @@ class TSNDataSet(data.Dataset):
         self.modality = modality
         self.image_tmpl = image_tmpl
         self.transform = transform
-        self.random_shift = random_shift
         self.test_mode = test_mode
         self.num_class = len(set(self.csv['action_id']))
+        self.sampler = SegmentedSample(new_length, num_segments, test_mode, start_index)
 
-        if self.modality == 'RGBDiff':
+        if self.modality == 'depthDiff':
             self.new_length += 1  # Diff needs one more image to calculate diff
 
         self._parse_list()
@@ -62,49 +60,10 @@ class TSNDataSet(data.Dataset):
     def _parse_list(self):
         self.video_list = [VideoRecord(self.csv.iloc[i], self.data_path) for i in range(self.data_num)]
 
-    def _sample_indices(self, record):
-        """
-
-        :param record: VideoRecord
-        :return: list
-        """
-        # split the video into segments
-        # each segment contains average_duration frames
-        average_duration = (record.num_frames - self.new_length + 1) // self.num_segments
-        if average_duration > 0:
-            # 1. init the offset for each segment as the first frame of it
-            # 2. assign a random int value (0 to average_duration) to the offset
-            offsets = np.multiply(list(range(self.num_segments)), average_duration) + randint(average_duration,
-                                                                                              size=self.num_segments)
-        elif record.num_frames > self.num_segments:
-            offsets = np.sort(randint(record.num_frames - self.new_length + 1, size=self.num_segments))
-        else:
-            offsets = np.zeros((self.num_segments,))
-        return offsets + 1
-
-    def _get_val_indices(self, record):
-        if record.num_frames > self.num_segments + self.new_length - 1:
-            tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
-            offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
-        else:
-            offsets = np.zeros((self.num_segments,))
-        return offsets + 1
-
-    def _get_test_indices(self, record):
-
-        tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
-
-        offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
-
-        return offsets + 1
-
     def __getitem__(self, index):
-        record = self.video_list[index]
 
-        if not self.test_mode:
-            segment_indices = self._sample_indices(record) if self.random_shift else self._get_val_indices(record)
-        else:
-            segment_indices = self._get_test_indices(record)
+        record = self.video_list[index]
+        segment_indices = self.sampler(record.num_frames)
 
         return self.get(record, segment_indices)
 
