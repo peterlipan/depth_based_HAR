c4052b5620bbedf81d433adb180528ce1cd32398
johnP-Li <847041172@qq.com>
2023-05-29 17:45:00 +0800

    bugs fixing

--- utils/losses.py
+++ utils/losses.py
@@ -4,12 +4,13 @@ import torch.nn as nn
 class HogRegressionLoss(nn.Module):
     def __init__(self):
         super(HogRegressionLoss, self).__init__()
+        self.log_softmax = nn.LogSoftmax(dim=-1)
         self.softmax = nn.Softmax(dim=-1)
         self.criterion = nn.KLDivLoss(reduction='sum')
 
     def forward(self, hog_preds, hog_features):
         assert hog_preds.size() == hog_features.size()
-        hog_preds = self.softmax(hog_preds)
+        hog_preds = self.log_softmax(hog_preds)
         hog_features = self.softmax(hog_features)
 
         return self.criterion(hog_preds, hog_features)
