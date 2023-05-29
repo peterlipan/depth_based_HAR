46add7a9e513dd74baabba189d21fef2801019da
johnP-Li <847041172@qq.com>
2023-05-29 16:08:26 +0800

    bugs fixing

--- utils/training.py
+++ utils/training.py
@@ -71,16 +71,6 @@ def train(loaders, model, criterion, optimizer, scheduler, logger, args):
             batch_time.update(time.time() - start)
             start = time.time()
 
-            cur_lr = optimizer.param_groups[-1]["lr"]
-            print(('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
-                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                   'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                    epoch, i, len(train_loader), batch_time=batch_time,
-                    data_time=data_time, loss=losses, top1=top1, top5=top5, lr=cur_lr)))
-
             cur_iter += 1
             if cur_iter % 150 == 0:
                 cur_lr = optimizer.param_groups[-1]["lr"]
